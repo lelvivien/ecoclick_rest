ce6cf42a4b93b4654b76cba051d2e2754ce224d1
soufiane <souphianezahiri@gmail.com>
2023-12-22 23:51:38 +0100

    rectified

--- public/index.php
+++ public/index.php
@@ -1,15 +1,29 @@
- <?php
+<?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathMiddleware;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("ello, $name");
+// Add Slim routing middleware
+$app->addRoutingMiddleware();
+
+// Set the base path to run the app in a subdirectory.
+// This path is used in urlFor().
+$app->setBasePath("/ecoclick_rest/public");
+
+$app->addErrorMiddleware(true, true, true);
+
+// Define app routes
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello, World!');
     return $response;
-});
-$app->run(); 
+})->setName('root');
+
+// Run app
+$app->run();
+?> 
